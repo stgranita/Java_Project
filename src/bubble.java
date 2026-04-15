f92aaf1454c1f76add2d9e9fe25c29d0d40ce8f5
PRANEEL KUMAR MUKHERJEE <PRANEEL@Priyams-MacBook-Pro.local>
2026-04-14 22:31:02 -0400

    changed bubble

--- src/bubble.java
+++ src/bubble.java
@@ -1,10 +1,10 @@
 public class bubble {
     public static void main(String[] args) {
-        String[] arr1 = {"Hello", "My", "World"};
-        String[] arr2 = {"Ranita's", "Java", "Programming", "Tips"};
-        String[] result = new String[arr1.length+arr2.length];
-        for (int i = 0;i<arr1.length;i++) result[i] = arr1[i];
-        for (int j = 0;j<arr2.length;j++) result [j+arr1.length] = arr2[j];
-        for (String s:result) System.out.println(s + ",");
+      int number =12321; boolean flag = true;
+      String s = String.valueOf(number);
+       for( int i = 0, j=s.length()-1;i<j;i++,j--) {
+          if (s.charAt(i) != s.charAt(j)) { flag = false; break;}
+        }
+       if (flag==true) System.out.println("Pallindrome"); else System.out.println("Not Pallindrome!");
     }
 }
